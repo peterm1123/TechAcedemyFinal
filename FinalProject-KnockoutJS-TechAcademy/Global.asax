553064826df1ac06aac797891ea5446d0e239cbf
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-21 17:31:10 -0800

    Add files via upload

---
+++ FinalProject-KnockoutJS-TechAcademy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProject_KnockoutJS_TechAcademy.MvcApplication" Language="C#" %>
---
+++ FinalProject-KnockoutJS-TechAcademy/Models/sampleProductCategories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject_KnockoutJS_TechAcademy.Models
+{
+    public class sampleProductCategories
+    {
+        
+    }
+}
